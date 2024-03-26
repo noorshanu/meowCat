6f99f8d8a2b8b500d4383a143591a0a69ed6bcac
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-27 04:04:37 +0530

    Update Mission.jsx

--- src/sections/Mission.jsx
+++ src/sections/Mission.jsx
@@ -12,7 +12,7 @@ function Mission() {
           <h1 className=" font-kid font-bold text-3xl sm:text-5xl my-3">$MCAT Mission</h1>
 
           <p className=" text-lg sm:text-xl "> 
-            To the moon we aim, no bumps, just zoom! It's NubShib moon day, a
+            To the moon we aim, no bumps, just zoom! It's  moon day, a
             NubShib celebration in full sway. Yes, yes, to the moon NubShib
             flies, today's the day, no lies. So, you in for this moon ride?
           </p>
