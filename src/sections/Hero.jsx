8eab9227f0259ace79c1328112ddcf899f9051df
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-27 03:58:48 +0530

    hhh

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,14 +19,14 @@ function Hero() {
 
         <div className=" flex justify-center gap-5 items-center">
           <a
-            href="https://twitter.com/MEOWCATSolana"
+            href="https://twitter.com/Mew_Catt_"
             className=" text-xl text-black bg-[#c74786] hover:scale-125 hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
           >
             <BsTwitterX />
           </a>
 
           <a
-            href="https://t.me/MEOWCAT"
+            href="https://t.me/meowcatoff"
             target="_blank"
             className=" text-xl text-black hover:scale-125 bg-[#c74786] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
           >
@@ -34,7 +34,7 @@ function Hero() {
           </a>
 
           <a
-            href="https://www.dextools.io/app/en/solana/pair-explorer/9U8yBLHEsFcqJLQiyrvi8tHYJ4haAs8sAnYrdxvxFfk1?t=1711488328507"
+            href="https://www.dextools.io/app/en/solana/pair-explorer/9U8yBLHEsFcqJLQiyrvi8tHYJ4haAs8sAnYrdxvxFfk1?t=1711470122933"
             target="_blank"
             className=" bg-[#c74786] rounded-full text-xl font-semibold p-1 border-2 hover:scale-125 "
           >
