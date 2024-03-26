d990ee521ae5554cbd72c6458f2df8e810f29c06
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-27 02:59:24 +0530

    new

---
+++ src/AppLayout.jsx
@@ -0,0 +1,99 @@
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+// import React, { useState, useRef } from "react";
+import Intro from "./sections/Intro";
+import MemeMark from "./sections/MemeMark";
+
+import Footer from "./sections/Footer";
+// import Music from "./nubshiba.mp3";
+import Mission from "./sections/Mission";
+
+// const Popup = ({ onClose }) => {
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-lg">
+//       <div className="rounded-lg p-8">
+//         <button
+//           className="bg-[#c74786] rounded-full px-6 py-2 text--2xl sm:text-5xl font-kid  font-bold border-2 hover:scale-125"
+//           onClick={onClose}
+//         >
+//           Nubshib - $NUBS
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+function AppLayout() {
+  // const [showPopup, setShowPopup] = useState(true);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const audioRef = useRef(null);
+
+  // const handleClosePopup = () => {
+  //   setShowPopup(false);
+  //   // Start playing music here
+  //   const audio = audioRef.current;
+  //   audio.play();
+  //   setIsPlaying(true);
+  // };
+
+  // const handlePlayPause = () => {
+  //   const audio = audioRef.current;
+  //   if (isPlaying) {
+  //     audio.pause();
+  //     setIsPlaying(false);
+  //   } else {
+  //     audio.play();
+  //     setIsPlaying(true);
+  //   }
+  // };
+
+  return (
+    <>
+      {/* {showPopup && <Popup onClose={handleClosePopup} />} */}
+      <main className=" bg-main pb-4 ">
+        <Navbar />
+
+        <Hero />
+        {/* <div className=" flex justify-center my-4 ">
+          <button
+            className="bg-[#c74786] text-black font-bold py-2 px-16 rounded focus:outline-none focus:shadow-outline  hover:scale-125"
+            onClick={handlePlayPause}
+          >
+            {isPlaying ? "Pause " : "Play Music"}
+          </button>
+        
+        </div> */}
+        <a href="https://solscan.io/token/H9oU2xVGUbiU9b8HWRudEHgFTL11tmyUzNscPhbp7TFc" target="_blank"
+         className="bg-[#c74786]  text-black font-bold py-2 px-2 sm:px-16 border-2  sm:text-2xl text-xs rounded-full   flex justify-center w-[360px] sm:w-[680px] mx-auto">H9oU2xVGUbiU9b8HWRudEHgFTL11tmyUzNscPhbp7TFc</a>
+      
+
+      
+      </main>
+
+      {/* <MemeMark /> */}
+
+      <div>
+        <dl className="h-[50px]" />
+        <Intro />
+        <dl className="h-[50px]" />
+
+        <Mission/>
+        {/* <div>
+          <Photo/>
+        </div> */}
+        {/* <div className=" bg-[#ffffff]">
+        <MemeCards />
+        </div> */}
+        {/* <div>
+          <RoadMap/>
+        </div> */}
+        {/* <div>
+          <Partner />
+        </div> */}
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default AppLayout;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,49 @@
+import { BsTwitterX, BsInstagram, BsFacebook } from "react-icons/bs";
+import { FaLinkedinIn, FaTelegram } from "react-icons/fa6";
+
+function Hero() {
+  return (
+    <section className=" pt-4  pb-[1rem]">
+      <div className="container-wrapper flex justify-center flex-col ">
+        <h1 className=" font-kid text-[80px] sm:text-[160px] font-bold text-center mt-[5rem] sm:mt-[3rem] uppercase">
+          meowcat
+        </h1>
+
+        <div className=" flex justify-center mx-auto -mt-[4rem]">
+          <img
+            src="images/hero.png"
+            alt=""
+            className="w-auto h-64    lg:h-[400px]  mx-auto "
+          />
+        </div>
+
+        <div className=" flex justify-center gap-5 items-center">
+          <a
+            href="https://twitter.com/NubshibSolana"
+            className=" text-xl text-black bg-[#c74786] hover:scale-125 hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
+          >
+            <BsTwitterX />
+          </a>
+
+          <a
+            href="https://t.me/NubShib"
+            target="_blank"
+            className=" text-xl text-black hover:scale-125 bg-[#c74786] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
+          >
+            <FaTelegram />
+          </a>
+
+          <a
+            href="https://www.dextools.io/app/en/solana/pair-explorer/9U8yBLHEsFcqJLQiyrvi8tHYJ4haAs8sAnYrdxvxFfk1?t=1711488328507"
+            target="_blank"
+            className=" bg-[#c74786] rounded-full text-xl font-semibold p-1 border-2 hover:scale-125 "
+          >
+            <img src="images/dext.png" alt="" className=" h-[29px] w-auto" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
