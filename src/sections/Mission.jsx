137eb8dd3a9a89dbea653a3e9a79ffb058c22739
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-27 04:04:22 +0530

    Update Mission.jsx

--- src/sections/Mission.jsx
+++ src/sections/Mission.jsx
@@ -9,7 +9,7 @@ function Mission() {
         </div>
 
         <div className=" bg-white border-2   py-8 px-4 rounded-2xl shadow-xl max-w-2xl ">
-          <h1 className=" font-kid font-bold text-3xl sm:text-5xl my-3">$NUBS Mission</h1>
+          <h1 className=" font-kid font-bold text-3xl sm:text-5xl my-3">$MCAT Mission</h1>
 
           <p className=" text-lg sm:text-xl "> 
             To the moon we aim, no bumps, just zoom! It's NubShib moon day, a
